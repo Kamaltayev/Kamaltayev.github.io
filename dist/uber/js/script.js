4ec121cdf26fdf68e0d8bf78b3f2a8584c5b9e81
Sam <kamaltaevs@gmail.com>
2024-08-15 22:20:50 +0600

    Add large files from dist directory to Git LFS

--- dist/uber/js/script.js
+++ dist/uber/js/script.js
@@ -1,17 +1,3 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('.menu'),
-    menuItem = document.querySelectorAll('.menu_item'),
-    hamburger = document.querySelector('.hamburger');
-
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('hamburger_active');
-        menu.classList.toggle('menu_active');
-    });
-
-    menuItem.forEach(item => {
-        item.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        })
-    })
-})
+version https://git-lfs.github.com/spec/v1
+oid sha256:cf6a917c916ff78a1d5039a31db7ae94713a70f7752366ddfd9c1c8536943e56
+size 580
